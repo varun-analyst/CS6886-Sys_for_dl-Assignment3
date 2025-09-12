741d1c053ee12bbd603973707a8541414a7338a7
Rajshekhar Rakshit <44888331+DextroLaev@users.noreply.github.com>
2025-09-13 00:51:54 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,17 +1,14 @@
 import torch 
 import torch.nn as nn
 
-def evaluate(model, testloader, device,type='ann'):
+def evaluate(model, testloader, device):
     correct = 0
     total = 0
     model.eval()
     with torch.no_grad():
         for batch_idx, (inputs, targets) in enumerate((testloader)):
             inputs = inputs.to(device)
-            if type == 'snn':
-                outputs = model(inputs).mean(0)
-            else:
-                outputs = model(inputs)
+            outputs = model(inputs)
             _, predicted = outputs.cpu().max(1)
             total += float(targets.size(0))
             correct += float(predicted.eq(targets).sum().item())
